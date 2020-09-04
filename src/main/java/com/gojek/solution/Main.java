f0bfe54c2d778b7fbbf6ba4fbe9c69dd7ac12b4b
varunchoudhary <varun.choudhary@limeroad.com>
2020-09-04 21:58:03 +0530

    add modes of setup file/interactive

--- src/main/java/com/gojek/solution/Main.java
+++ src/main/java/com/gojek/solution/Main.java
@@ -1,5 +1,6 @@
 package com.gojek.solution;
 
+import com.gojek.solution.command.CommandFactory;
 import com.gojek.solution.exceptions.InvalidModeException;
 import com.gojek.solution.service.ParkingLotService;
 
@@ -7,10 +8,12 @@ public class Main {
     public static void main(String[] args) {
         final OutputPrinter outputPrinter = new OutputPrinter();
         final ParkingLotService parkingLotService = new ParkingLotService();
+        final CommandFactory commandFactory =
+                new CommandFactory(parkingLotService);
         if (isInteractiveMode(args)) {
-
+            new InteractiveMode(commandExecutorFactory, outputPrinter).process();
         } else if (isFileInputMode(args)) {
-
+            new FileMode(commandExecutorFactory, outputPrinter, args[0]).process();
         } else {
             throw new InvalidModeException();
         }
---
+++ src/main/java/com/gojek/solution/command/CommandFactory.java
@@ -0,0 +1,9 @@
+package com.gojek.solution.command;
+
+import com.gojek.solution.service.ParkingLotService;
+
+public class CommandFactory {
+    public CommandFactory(ParkingLotService parkingLotService) {
+
+    }
+}
