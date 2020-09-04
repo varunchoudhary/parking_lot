000b7a2dd6a8a460dccb0e84c0e1eabc83737289
varunchoudhary <varun.choudhary@limeroad.com>
2020-09-05 01:00:41 +0530

    add colored car registration

---
+++ src/main/java/com/gojek/solution/command/ColorToSlotNumberCommandExecutor.java
@@ -0,0 +1,34 @@
+package com.gojek.solution.command;
+
+import com.gojek.solution.OutputPrinter;
+import com.gojek.solution.exceptions.ParkingLotException;
+import com.gojek.solution.model.Command;
+import com.gojek.solution.model.Slot;
+import com.gojek.solution.service.ParkingLotService;
+
+import java.util.List;
+
+public class ColorToSlotNumberCommandExecutor extends CommandExecutor {
+    public ColorToSlotNumberCommandExecutor(ParkingLotService parkingLotService, OutputPrinter outputPrinter) {
+        super(parkingLotService,outputPrinter);
+    }
+
+    @Override
+    public boolean validate(Command command) {
+        return command.getParams().size() == 1;
+    }
+
+    @Override
+    public void execute(Command command) throws ParkingLotException {
+        final List<Slot> slotsForColor = parkingLotService.getSlotsForColor(command.getParams().get(0));
+        if(slotsForColor.isEmpty()){
+            outputPrinter.notFound();
+        }else{
+            StringBuilder stringBuilder = new StringBuilder();
+            for(Slot slot:slotsForColor)
+                if(slot.getParkedCar().getColor().equals(command.getParams().get(0)))
+                    stringBuilder.append(slot.getParkedCar().getRegistrationNumber());
+            outputPrinter.printWithNewLine(stringBuilder.toString());
+        }
+    }
+}
