77124ba63a2cf916c50349bd78eabb03f29b474d
varunchoudhary <varun.choudhary@limeroad.com>
2020-09-05 12:56:46 +0530

    add strategy based test cases

--- src/test/java/com/gojek/solution/strategy/NaturalOrderingParkingStrategyTest.java
+++ src/test/java/com/gojek/solution/strategy/NaturalOrderingParkingStrategyTest.java
@@ -1,5 +1,6 @@
 package com.gojek.solution.strategy;
 
+import com.gojek.solution.exceptions.NoFreeSlotAvailableException;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
@@ -7,5 +8,24 @@ import static org.junit.Assert.*;
 public class NaturalOrderingParkingStrategyTest {
     private NaturalOrderingParkingStrategy naturalOrderingParkingStrategy =
             new NaturalOrderingParkingStrategy();
+
+    @Test
+    public void testValidStrategyExecution() {
+        naturalOrderingParkingStrategy.addSlot(1);
+        naturalOrderingParkingStrategy.addSlot(2);
+        naturalOrderingParkingStrategy.addSlot(3);
+        assertEquals((Integer)1, naturalOrderingParkingStrategy.getNextSlot());
+        naturalOrderingParkingStrategy.removeSlot(2);
+        assertEquals((Integer)1, naturalOrderingParkingStrategy.getNextSlot());
+        naturalOrderingParkingStrategy.removeSlot(1);
+        assertEquals((Integer)3, naturalOrderingParkingStrategy.getNextSlot());
+        naturalOrderingParkingStrategy.addSlot(2);
+        assertEquals((Integer)2, naturalOrderingParkingStrategy.getNextSlot());
+    }
+
+    @Test(expected = NoFreeSlotAvailableException.class)
+    public void testFullParkingStrategy() {
+        Integer nextSlot = naturalOrderingParkingStrategy.getNextSlot();
+    }
 
 }
